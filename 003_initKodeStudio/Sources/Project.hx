4a02028f5f638e2efa0674d20e012618280a615a
Lewis Lepton <lewisleptondev@gmail.com>
2016-03-24 23:25:10 +0000

    added '003 init kode studio'

---
+++ 003_initKodeStudio/Sources/Project.hx
@@ -0,0 +1,16 @@
+package;
+
+import kha.Framebuffer;
+
+class Project {
+	public function new() {
+		
+	}
+
+	public function update():Void {		
+	}
+
+	public function render(framebuffer:Framebuffer):Void {
+		
+	}
+}
---
+++ 003_initKodeStudio/khafile.js
@@ -0,0 +1,4 @@
+var project = new Project('003_initKodeStudio');
+project.addAssets('Assets/**');
+project.addSources('Sources');
+return project;
