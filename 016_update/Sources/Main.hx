0af4d8d4b93f45cb204c9642d7f2ab8f7647d633
Lewis Lepton <lewisleptondev@gmail.com>
2016-04-16 16:42:07 +0100

    updated 016_update

--- 016_update/Sources/Main.hx
+++ 016_update/Sources/Main.hx
@@ -6,7 +6,7 @@ import kha.Assets;
 
 class Main {
 	public static function main() {
-		System.init({title:"Project", width:1024, height:768}, function(){
+		System.init({title:"016_update", width:1024, height:768}, function(){
 			Assets.loadEverything(function(){
 				var Project = new Project();
 				Scheduler.addTimeTask(Project.update, 0, 1 / 60);
