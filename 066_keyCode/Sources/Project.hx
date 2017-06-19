44e7f7d69bad539cf547b7b20a07e20420ceed87
Lewis Lepton <lewisleptondev@gmail.com>
2017-06-20 00:51:18 +0100

    added ‘066_keyCode’

---
+++ 066_keyCode/Sources/Project.hx
@@ -0,0 +1,46 @@
+package;
+
+import kha.Framebuffer;
+import kha.Color;
+import kha.Assets;
+import kha.input.KeyCode;
+import kha.input.Keyboard;
+
+class Project {
+	var YES:Bool;
+	var NO:Bool;
+	public function new() {
+		Keyboard.get().notify(onKeyDown, onKeyUp);
+	}
+
+	public function update():Void {
+		if (YES){
+			trace('YES');
+		} else if (NO){
+			trace('NO');
+		}
+	}
+
+	public function render(framebuffer:Framebuffer):Void {
+		var graphics = framebuffer.g2;
+		graphics.begin();
+		
+		graphics.end();
+	}
+
+	public function onKeyDown(keyCode:Int):Void {
+		switch (keyCode){
+			case KeyCode.A: YES = true;
+			case KeyCode.P: NO = true;
+		default: return;
+		}
+	}
+
+	public function onKeyUp(keyCode:Int):Void {
+		switch (keyCode){
+			case KeyCode.A: YES = false;
+			case KeyCode.P: NO = false;
+		default: return;
+		}
+	}
+}
---
+++ 066_keyCode/khafile.js
@@ -0,0 +1,4 @@
+let project = new Project('066_keyCode');
+project.addAssets('Assets/**');
+project.addSources('Sources');
+resolve(project);
