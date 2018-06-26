647ccfa697386882ea63f9ed74764d8548de4651
Lewis Lepton <lewisleptondev@gmail.com>
2018-06-26 15:53:01 +0100

    SPP to 2 - for windows users - thanks Kevin ;)

--- 079_forLoopDraw/Sources/Main.hx
+++ 079_forLoopDraw/Sources/Main.hx
@@ -5,9 +5,10 @@ import kha.Scheduler;
 import kha.Assets;
 
 class Main {
-public static var WIDTH = 1024;
-public static var HEIGHT = 768;
-public static var SPP = 3;
+	public static var WIDTH = 1024;
+	public static var HEIGHT = 768;
+	public static var SPP = 2;
+	
 	public static function main(){
 		System.init({
 			title:"079_forLoopDraw",
