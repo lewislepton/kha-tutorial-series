4fa3770a651010240aeb87751fe3fe1646336f88
Lewis Lepton <lewisleptondev@gmail.com>
2018-07-17 11:16:10 +0100

    added ‘087_runnerAI’

---
+++ 087_runnerAI/Sources/Main.hx
@@ -0,0 +1,26 @@
+package;
+
+import kha.System;
+import kha.Scheduler;
+import kha.Assets;
+
+class Main {
+	public static var WIDTH = 1024;
+	public static var HEIGHT = 768;
+	public static var SPP = 2;
+
+	public static function main(){
+		System.init({
+			title:'087_runnerAI',
+			width:WIDTH,
+			height:HEIGHT,
+			samplesPerPixel:SPP
+		}, function(){
+			Assets.loadEverything(function(){
+				var Project = new Project();
+				Scheduler.addTimeTask(Project.update, 0, 1 / 60);
+				System.notifyOnRender(Project.render);
+			});
+		});
+	}
+}
---
+++ 087_runnerAI/khafile.js
@@ -0,0 +1,4 @@
+let project = new Project('087_runnerAI');
+project.addAssets('Assets/**');
+project.addSources('Sources');
+resolve(project);
