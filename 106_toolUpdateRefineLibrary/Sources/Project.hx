f9ea118d32c28543d211f1b6d8a32768497ba2a1
Lewis Lepton <lewisleptondev@gmail.com>
2018-12-27 10:07:25 +0000

    added ’106_toolUpdateRefineLibrary’

---
+++ 106_toolUpdateRefineLibrary/Sources/Project.hx
@@ -0,0 +1,22 @@
+package;
+
+import kha.Framebuffer;
+import kha.Color;
+import kha.Assets;
+
+class Project {
+	public function new(){
+		
+	}
+
+	public function update():Void {
+		
+	}
+
+	public function render(framebuffer:Framebuffer):Void {
+		var graphics = framebuffer.g2;
+		graphics.begin();
+		
+		graphics.end();
+	}
+}
